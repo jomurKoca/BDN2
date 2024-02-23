b13724d881e50c5c3e60b7c09e82d340725a85c3
Ömür Koca <93597251+jomurKoca@users.noreply.github.com>
2024-02-23 16:57:24 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String [] args){
+
+        Scanner input = new Scanner(System.in);
+        //int Physik, Mathe, Chemie, Deutsch ;
+        // double Durchschnitt;
+
+        System.out.println("Gib Physik Note ein  : ");
+
+        int Physik =input.nextInt();
+
+        System.out.println("Gib Mathe Note ein  : ");
+        int Mathe=input.nextInt();
+
+        System.out.println("Gib Chemie Note ein  : ");
+        int Chemie = input.nextInt();
+
+        System.out.println("Gib Deutsch Note ein  : ");
+        int Deutsch=input.nextInt();
+
+
+        double Durchschnitt = ((Physik+Mathe+Chemie+Deutsch)/4);
+        System.out.println("Ergebnis : " + (Durchschnitt<50 ? "Durcfallen:/" : "Bestanden! :)"));
+
+
+
     }
 }
